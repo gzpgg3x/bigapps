4bfcde34f9ea78e5b326400c1e0b4d95ae926cbe
Forrest Pan <gzpgg3x@yahoo.com>
2013-05-04 17:53:33 -0400

    gotTitleIsbn

--- boocGoer/bestsell/api.py
+++ boocGoer/bestsell/api.py
@@ -8,6 +8,13 @@ from bestsell.models import *
 import json
 from datetime import datetime
 
+import urllib2
+#from urllib2 import urlopen
+from json import loads
+import codecs, sys
+import os 
+sys.stdout = codecs.getwriter('UTF-8')(sys.stdout)
+
 @csrf_exempt
 @require_POST
 def new_shout(request):
@@ -15,6 +22,45 @@ def new_shout(request):
     lng = request.POST['lng']
     booklist = request.POST['booklist']
     bldate = request.POST['bldate']
+
+    url = "http://api.nytimes.com/svc/books/v2/lists/" + bldate + "/" + booklist + "?&offset=&sortby=&sortorder=&api-key=1ddfd825a1084dbe50334ad25dd5ddfa:2:67632807"
+
+    # response = urllib2.urlopen(url)
+    # articles = response.read()
+    # print articles
+
+
+
+    req = urllib2.Request(url)
+    opener = urllib2.build_opener()
+    f = opener.open(req)
+    articles = json.loads(f.read())
+    print articles
+
+
+    g=""
+    # f = codecs.open('output.txt', 'w', encoding='utf-8')
+    for story in articles["results"]:
+        # f.write(story["title"] + "\n")
+        b = 0
+        for book in story["isbns"]:
+            if b == 0: 
+                try:
+                    # g = g + story["title"] + "\n"
+                    g = g + book["isbn10"] + "\n"
+                    b = b + 1
+                except:
+                    g = g + " \n"
+            
+
+        for book in story["book_details"]: 
+            try:
+                # g = g + story["title"] + "\n"
+                g = g + book["title"] + "\n"
+            except:
+                g = g + " \n"
+    # f.close()
+    print g
 
     shout = Shout.objects.create(lat=lat,lng=lng,booklist=booklist,bldate=bldate)
 
@@ -27,6 +73,14 @@ def new_shout(request):
     }
     
     return HttpResponse(json.dumps(response))
+
+
+
+    # def call_the_articles(url):
+    # #      url = "http://api.nytimes.com/svc/books/v2/lists/" + bldate + "/" + booklist + "?&offset=&sortby=&sortorder=&api-key=1ddfd825a1084dbe50334ad25dd5ddfa:2:67632807"
+    #      return loads(urlopen(url).read()) 
+
+
 
 def get_shouts(request):
     lat = float(request.GET['lat'])
